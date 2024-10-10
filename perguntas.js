8c867069d530b3acd99fb84a013b68fd1991e148
mariananeves13 <161324884+mariananeves13@users.noreply.github.com>
2024-10-10 08:00:11 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'biologia',
+    'Quantos ossoa tem o corpo humano?',
+    '206 ossos'
+)
+
+criaCartao(
+    'biologia',
+    'Qual o menor e o maior osso do corpo?',
+    ' fêmur'
+)
+
+criaCartao(
+    'biologia',
+    'Quantas células existem no corpo humano?',
+    '30 trilhões'
+)
+
+criaCartao(
+    'biologia',
+    'Quantos fios de cabelo tem na cabeça de uma pessoa?',
+    '150 mil fios de cabelo'
+)
+
+criaCartao( 
+    'biologia',
+    'Qual orgão é responsável pelo bombeamento do sangue?',
+    'O coração!'
+)
+
+criaCartao( 
+    'categoria',
+    'pergunta',
+    'resposta'
+)
